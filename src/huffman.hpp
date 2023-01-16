86ad3528e466c90d25990ba8b363d60d599ddbbb
lochambeno <louis.chambenoit@outlook.fr>
2023-01-16 15:02:19 +0100

    Creation des fichiers sources et du CMake. Création du .gitignore et des fichiers de tests.

---
+++ src/huffman.hpp
@@ -0,0 +1,9 @@
+#ifndef AAC_ALGORITHMS_HUFFMAN_HPP
+#define AAC_ALGORITHMS_HUFFMAN_HPP
+
+#include "tree.hpp"
+
+class Huffman{
+};
+
+#endif
---
+++ src/tree.hpp
@@ -0,0 +1,11 @@
+#ifndef AAC_ALGORITHMS_TREE_HPP
+#define AAC_ALGORITHMS_TREE_HPP
+
+#include <vector>
+
+class Tree{
+    private:
+
+};
+
+#endif
